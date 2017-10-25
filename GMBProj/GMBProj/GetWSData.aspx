1e4be239291ed5b86210b5ba9feb2db2c41a9664
Tarcisio Gambin <gambin.br@gmail.com>
2017-10-25 20:09:36 -0200

    Criacao de projeto Asp.Net 'classico' com um WebService WCF expondo metodos para calculos que sao chamados atraves da pagina GetWSData.aspx

---
+++ GMBProj/GMBProj/GetWSData.aspx
@@ -0,0 +1,28 @@
+﻿<%@  title="Teste" language="C#" masterpagefile="~/Site.Master" autoeventwireup="true" codebehind="GetWSData.aspx.cs" inherits="GMBProj.GetWSData" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+
+    <div class="wrapper">
+        <div class="l-field">Entre com os valores para realizar o cálculo:</div>
+        <div class="i-field">
+            <p>
+                <asp:TextBox ID="num1" runat="server"></asp:TextBox>
+            </p>
+            <p>
+                <asp:TextBox ID="num2" runat="server"></asp:TextBox>
+            </p>
+            <p>
+                <asp:DropDownList ID="sOperacao" runat="server"></asp:DropDownList></p>
+            <p>
+                <asp:Button ID="calcularBtn" runat="server" Text="Calcular" />
+            </p>
+        </div>
+    </div>
+    <div class="q-results">
+        <p>
+            Total:
+            <asp:Label ID="totalizador" runat="server" Text=""></asp:Label>
+        </p>
+
+    </div>
+
+</asp:Content>
---
+++ GMBProj/GMBProj/GetWSData.aspx.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml;
+using GMBProj.CalcService;
+
+namespace GMBProj
+{
+    public partial class GetWSData : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            // DropDownList tsOperacao = (DropDownList)ViewState["sOperacao"];
+            if (sOperacao.Items.Count == 0)
+            {
+                // sOperacao.Items.Clear();
+                foreach (int value in Enum.GetValues(typeof(calcOptions)))
+                {
+                    var name = Enum.GetName(typeof(calcOptions), value);
+                    sOperacao.Items.Add(name);
+                }
+            }
+            calcularBtn.Click += new EventHandler(this.calcularBtn_Click);
+        }
+
+        public void calcularBtn_Click(Object sender, EventArgs e)
+        {
+            CalculatorClient client = new CalculatorClient();
+
+            int n1 = int.Parse(num1.Text, CultureInfo.InvariantCulture);
+            int n2 = int.Parse(num2.Text, CultureInfo.InvariantCulture);
+            double total = 0;
+
+            string query = sOperacao.Text;
+
+            //Response.Write(num1 + num2);
+            switch (sOperacao.SelectedValue)
+            {
+                case "AddNumbers":
+                    {
+                        total = double.Parse(client.AddNumbers(n1, n2).ToString("F2"), CultureInfo.InvariantCulture);
+                        break;
+                    }
+                case "SubstractNumbers":
+                    {
+                        total = double.Parse(client.SubstractNumbers(n1, n2).ToString("F2"), CultureInfo.InvariantCulture);
+                        break;
+                    }
+                case "MultiplyNumbers":
+                    {
+                        total = double.Parse(client.MultiplyNumbers(n1, n2).ToString("F2"), CultureInfo.InvariantCulture);
+                        break;
+                    }
+                case "DivisionNumbers":
+                    {
+                        total = double.Parse(client.DivisionNumbers(n1, n2).ToString("F2"), CultureInfo.InvariantCulture);
+                        break;
+                    }
+            }
+            totalizador.Text = total.ToString("F2");
+        }
+    }
+
+
+    public enum calcOptions
+    {
+        AddNumbers,
+        SubstractNumbers,
+        MultiplyNumbers,
+        DivisionNumbers
+    }
+}
